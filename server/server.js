5f5376322467eac928ce81f09c464efe9fa80c02
Darian N <darian.nasrabadi@gmail.com>
2018-01-19 16:49:20 -0600

    added base files and formed server.js code

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const galleryRouter = require('./routes/gallery.js');
+
+const app = express();
+let port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+
+// routes
+app.use('/gallery', galleryRouter);
+
+app.listen(port, function () {
+    console.log('On Port: ', port);
+});
