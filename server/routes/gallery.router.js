1aeee1bdc4c5b4ff036b213770b9cc13e6c5ccce
Darian N <darian.nasrabadi@gmail.com>
2018-01-20 16:48:07 -0600

    add fullstack view counter dom to client to server to database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -19,7 +19,7 @@ router.put('/:id', (req, res) => {
     let count = req.body.like_count + 1
     let queryText = `UPDATE images
                      SET like_count = $1
-                     WHERE id = $2`;
+                     WHERE id = $2;`;
     pool.query(queryText, [count, req.params.id])
         .then((result) => {
             res.sendStatus(200);
@@ -28,6 +28,38 @@ router.put('/:id', (req, res) => {
             console.log('Error making update query', err);
             res.sendStatus(500);
         });
+});
+
+
+router.put('/view/:id', (req, res) => {
+    let block = req.body
+    if (block.image_display === true) {
+        let count = req.body.view_count + 1
+        let queryText = `UPDATE images
+                         SET view_count = $1, image_display = false, text_display = true
+                         WHERE id = $2;`;
+        pool.query(queryText, [count, req.params.id])
+            .then((result) => {
+                res.sendStatus(200);
+            })
+            .catch((err) => {
+                console.log('Error making update query', err);
+                res.sendStatus(500);
+            });
+    }
+    else if (block.image_display === false) {
+        let queryText = `UPDATE images
+                         SET image_display = true, text_display = false
+                         WHERE id = $1;`;
+        pool.query(queryText, [req.params.id])
+            .then((result) => {
+                res.sendStatus(200);
+            })
+            .catch((err) => {
+                console.log('Error making update query', err);
+                res.sendStatus(500);
+            });
+    }
 });
 
 module.exports = router;
